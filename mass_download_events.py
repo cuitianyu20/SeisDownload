3de2a47c5f10cddaa3c9e34d399b46d3792c1f74
Tianyu Cui <68799423+cuitianyu20@users.noreply.github.com>
2023-12-28 12:28:25 +0800

    Update mass_download_events.py

--- mass_download_events.py
+++ mass_download_events.py
@@ -279,11 +279,11 @@ if __name__ == '__main__':
     station_name: "ANMO" or "TA01" or "ANMO,TA01" or "*"
     channel: channels (default: ["BHZ", "HHZ", "SHZ", "EHZ"])
     sta_range: 
-        domain type:0 (RectangularDomain) sta_range = [sta_lat_min, sta_lat_max, sta_lon_min, sta_lon_max] in degree
-                       if limit_distance=True, add distance restriction to the Rectangular domain
-                      (RestrictionDomain) [min_dis, max_dis] in degree 
-        domain type:1 (CircularDomain) sta_range = [minradius, maxradius] in degree 
+        domain type:0 (CircularDomain) sta_range = [minradius, maxradius] in degree 
                                        mid points: [ref_lat, ref_lon] in degree
+        domain type:1 (RectangularDomain) sta_range = [sta_lat_min, sta_lat_max, sta_lon_min, sta_lon_max] in degree
+               if limit_distance=True, add distance restriction to the Rectangular domain
+              (RestrictionDomain) [min_dis, max_dis] in degree 
         domain type:2 (GlobalDomain) []
     evt_range: [evt_lat_min, evt_lat_max, evt_lon_min, evt_lon_max] in degree (lon: 0 degree ~ 360 degree)
     evt_mag_range: [evt_mag_min, evt_mag_max]

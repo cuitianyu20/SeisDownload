70d5702b21c676ecad0aded3304a0b7204a769f3
Tianyu Cui <68799423+cuitianyu20@users.noreply.github.com>
2024-01-09 11:44:36 +1100

    Update test_download.py

--- test/test_download.py
+++ test/test_download.py
@@ -4,7 +4,7 @@
 # import seisdownload module
 import seisdownload
 
-### seismic waveform parameter setting
+### seismic waveform parameter setting ###
 client_ini = "IRIS" # search event from this client, but search all station records from all clients
 starttime = "2007-09-01 00:00:00"
 endtime = "2007-09-01 23:59:59"
@@ -32,18 +32,18 @@ evt_min_mag = 5.0           # min event magnitude
 evt_max_mag = None          # max event magnitude
 ### event catalog file setting  ###
 event_catalog = "event_with_PKiKP.csv"
-# station catalog setting (array, station name, distance limit) ###
+### station catalog setting (array, station name, distance limit) ###
 array_name = "*"            # network name
 station_name = "*"          # station name
 sta_min_dis_limit = 0       # epicentral distance min limit (rect station domain)
 sta_max_dis_limit = 180     # epicentral distance max limit (rect station domain)
-### other setting
+### other setting ###
 event_circ_center = True    # center point is event location or not
 rect_distance_limit = False # use distance limit or not (rect station domain)
 delete_mseed = True         # delete raw miniseed file or not
 remove_response = True      # remove response or not
 
-# download seismic data
+### download seismic data ###
 seisdownload.massdownload_data(starttime, endtime, channel, wave_len, evt_domain_type, evt_domain_range, sta_domain_type, sta_domain_range, 
                     evt_min_dep, evt_max_dep, evt_min_mag, evt_max_mag,event_catalog, client_ini, array_name, station_name, 
                     sta_min_dis_limit, sta_max_dis_limit, event_circ_center, rect_distance_limit, delete_mseed, remove_response)
